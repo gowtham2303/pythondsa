1120091e9f3a5daf9cbb0830225aa876bd368c54
Gowtham <114149222+gowtham2303@users.noreply.github.com>
2024-07-15 10:03:13 +0530

    Time: 41 ms (35.12%) | Memory: 12.6 MB (73.40%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,10 +4,27 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        profits=[]
+        nums=prices
+        profits=0
+        for i in range(1,len(nums)):
+            if nums[i]-nums[i-1]>0:
+                profits+=nums[i]-nums[i-1]
+        return profits
+
+
+
+     
+
+
+
+
+
+
+
+
+
+        profit = 0
         for i in range (1,len(prices)):
             if prices[i]>prices[i-1]:
-                profits.append(prices[i]-prices[i-1])
-        k=sum(profits)
-        
-        return k
+                profit+=prices[i]-prices[i-1]
+        return profit 
